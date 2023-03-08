48c137d50975ecc8586d6124543ee20112ec7568
Suhrob <mlkv.deb@gmail.com>
2023-03-08 11:38:08 +0500

    Initial

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as _UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(_UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("phone", "password", "type")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    # "groups",
+                    # "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phone", "password1", "password2"),
+            },
+        ),
+    )
+
+    list_display = ("id", "phone", "first_name", "last_name", "is_staff")
+    search_fields = ("phone", "first_name", "last_name")
+    ordering = ("phone",)
