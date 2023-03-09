9562272fba153e5b0aa4fe042ca558c78e2769a3
Suhrob <mlkv.deb@gmail.com>
2023-03-09 22:19:53 +0500

    get_webhook_url function fixed

--- bot/helpers.py
+++ bot/helpers.py
@@ -4,6 +4,6 @@ from django.urls import reverse
 
 def get_webhook_url():
     host: str = settings.HOST
-    if not host.endswith("/"):
-        host += "/"
+    if host.endswith("/"):
+        host = host[:-1]
     return host + reverse(settings.BOT_WEBHOOK_PATH, args=(settings.BOT_TOKEN,))
